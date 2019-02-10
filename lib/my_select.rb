83c3925b4a6603d764815378a46aeb429ba624f6
Zoe Bai <s.xue.bai@gmail.com>
2019-02-10 02:15:21 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,5 @@
+=begin
+# solution 1
 collection = [1, 2, 3, 4, 5] 
 def my_select(collection)
  array = []
@@ -13,4 +15,22 @@ end
 
 my_select(collection) do |num|
   num.even?
+end
+=end
+
+
+
+# solution 2
+def my_select(collection)
+  array = []
+  collection.each do |num|
+    if yield(num) == true
+       array << num
+     end
+  end
+  array
+end
+
+my_select([1, 2, 3, 4, 5]) do |number|
+  number.even?
 end
